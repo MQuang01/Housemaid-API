62f9246a4429ebeaa0f44091c87dd89dd5375674
Quang <hcmquang2111@gmail.com>
2024-03-20 15:07:30 +0700

    crud-feedback

---
+++ src/main/java/com/cghue/projecthousemaidwebapp/service/IFeedBackService.java
@@ -0,0 +1,15 @@
+package com.cghue.projecthousemaidwebapp.service;
+
+import com.cghue.projecthousemaidwebapp.domain.FeedBack;
+import com.cghue.projecthousemaidwebapp.domain.dto.req.FeedBackReqDto;
+import com.cghue.projecthousemaidwebapp.domain.dto.res.FeedBackResDto;
+
+import java.util.List;
+
+public interface IFeedBackService {
+    List<FeedBackResDto> getAllFeedBacks();
+
+    boolean saveFeedBack(FeedBackReqDto feedBackReqDto);
+    boolean updateFeedBack(Long idFeedback, Long userId, String description, Integer percent);
+    boolean deleteFeedBack(Long id, Long userId);
+}
