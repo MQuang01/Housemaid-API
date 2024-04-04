e8c7dab70bd68c0f12e53be70d9ebbe4decc76d2
Nguyen Huu Hiep <nhhiepit@gmail.com>
2024-04-04 14:53:09 +0700

    Fix order bug

--- src/main/java/com/cghue/projecthousemaidwebapp/ProjectWebAppCleaningServiceApplication.java
+++ src/main/java/com/cghue/projecthousemaidwebapp/ProjectWebAppCleaningServiceApplication.java
@@ -10,4 +10,5 @@ public class ProjectWebAppCleaningServiceApplication {
 		SpringApplication.run(ProjectWebAppCleaningServiceApplication.class, args);
 	}
 
+
 }
--- src/main/java/com/cghue/projecthousemaidwebapp/restcontroller/OrderRestController.java
+++ src/main/java/com/cghue/projecthousemaidwebapp/restcontroller/OrderRestController.java
@@ -4,9 +4,6 @@ import com.cghue.projecthousemaidwebapp.domain.dto.req.OrderReqDto;
 import com.cghue.projecthousemaidwebapp.service.IOrderService;
 import jakarta.mail.MessagingException;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -40,8 +37,8 @@ public class OrderRestController {
 
     @GetMapping
     @PreAuthorize("hasAnyAuthority('ADMIN')")
-    public ResponseEntity<?> getAllOrder(@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
-        return ResponseEntity.ok(orderService.findAllOrder(pageable));
+    public ResponseEntity<?> getAllOrder() {
+        return ResponseEntity.ok(orderService.findAllOrder());
     }
 
     @PutMapping("/update-status/{id}")
